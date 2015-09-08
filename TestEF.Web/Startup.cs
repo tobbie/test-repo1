774ad964ad27082ca937676e74c6e8135bbdb6d6
Ouwatobi Akinseye <tobi.akinseye@gmail.com>
2015-09-08 21:51:48 +0100

    first commit to local

---
+++ TestEF.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestEF.Web.Startup))]
+namespace TestEF.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
